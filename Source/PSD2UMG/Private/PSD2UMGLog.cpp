1c57f1eee9994a33e2f9551bd873438bbc76d54a
Pavel Dyatlov <paul.dyatlov@gmail.com>
2026-04-08 15:15:46 +0200

    feat(02-02): declare LogPSD2UMG log category

---
+++ Source/PSD2UMG/Private/PSD2UMGLog.cpp
@@ -0,0 +1,5 @@
+// Copyright 2018-2021 - John snow wind
+
+#include "PSD2UMGLog.h"
+
+DEFINE_LOG_CATEGORY(LogPSD2UMG);
---
+++ Source/PSD2UMG/Public/PSD2UMGLog.h
@@ -0,0 +1,8 @@
+// Copyright 2018-2021 - John snow wind
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Logging/LogMacros.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogPSD2UMG, Log, All);

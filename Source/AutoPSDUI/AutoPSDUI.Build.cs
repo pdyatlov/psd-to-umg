0d53c2c30ae143b1639b69fb37984f78a999aa3b
Pavel Dyatlov <paul.dyatlov@gmail.com>
2026-04-08 11:07:47 +0200

    feat(01-01): update .uplugin and Build.cs for UE 5.7
    - Rename module to PSD2UMG, EngineVersion 5.7
    - PlatformAllowList replaces WhitelistPlatforms
    - Remove PythonScriptPlugin and EditorScriptingUtilities deps

--- Source/AutoPSDUI/AutoPSDUI.Build.cs
+++ Source/AutoPSDUI/AutoPSDUI.Build.cs
@@ -2,59 +2,32 @@
 
 using UnrealBuildTool;
 
-public class AutoPSDUI : ModuleRules
+public class PSD2UMG : ModuleRules
 {
-	public AutoPSDUI(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				"DeveloperSettings",
-				"PythonScriptPlugin",
-				"UnrealEd",
-				"UMGEditor",
-				"UMG",
-				"AssetRegistry",
-				"EditorScriptingUtilities",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+    public PSD2UMG(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "DeveloperSettings",
+                "UnrealEd",
+                "UMGEditor",
+                "UMG",
+                "AssetRegistry",
+            }
+        );
+    }
 }
